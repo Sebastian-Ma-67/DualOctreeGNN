fe53bdda1de9c0e0e5749c6d86b3f68f6c139545
Sebastian-Ma-67 <sebastian_xq_ma@163.com>
2022-07-31 14:01:31 +0000

    添加 config 参数的中文注释

--- solver/config.py
+++ solver/config.py
@@ -44,8 +44,8 @@ _C.SOLVER.progress_bar      = True
 
 
 # DATA related parameters
-_C.DATA = CN()
-_C.DATA.train = CN()
+_C.DATA = CN() # 嵌套使用
+_C.DATA.train = CN() # 嵌套使用
 _C.DATA.train.name          = ''          # The name of the dataset | 数据集的名称
 
 # For octree building
@@ -54,12 +54,12 @@ _C.DATA.train.name          = ''          # The name of the dataset | 数据集�
 # If node_dis = True and there are no normals, the feature is also 4 channels,
 # i.e., a 3 channel # displacement of average points relative to the center
 # points, and the last channel is constant.
-_C.DATA.train.depth         = 5           # The octree depth
-_C.DATA.train.full_depth    = 2           # The full depth
-_C.DATA.train.node_dis      = False       # Save the node displacement
-_C.DATA.train.split_label   = False       # Save the split label
+_C.DATA.train.depth         = 5           # The octree depth | 训练时的八叉树深度
+_C.DATA.train.full_depth    = 2           # The full depth | 啥是full depth？
+_C.DATA.train.node_dis      = False       # Save the node displacement | 啥是node_dis？
+_C.DATA.train.split_label   = False       # Save the split label 
 _C.DATA.train.adaptive      = False       # Build the adaptive octree
-_C.DATA.train.node_feat     = False       # Calculate the node feature
+_C.DATA.train.node_feat     = False       # Calculate the node feature | 是否计算节点特征？
 
 # For normalization
 # If radius < 0, then the method will compute a bounding sphere
@@ -68,58 +68,63 @@ _C.DATA.train.radius        = -1.         # The radius and center of the boundin
 _C.DATA.train.center        = (-1., -1., -1.)
 
 # For transformation
-_C.DATA.train.offset        = 0.016       # Used to displace the points when building octree
-_C.DATA.train.normal_axis   = ''          # Used to re-orient normal directions
+_C.DATA.train.offset        = 0.016       # Used to displace the points when building octree | 为啥构建八叉树的时候要displace points呢？
+_C.DATA.train.normal_axis   = ''          # Used to re-orient normal directions | 用于重新定向法向量的方向
+
+# MXQ 2022.07.31
+_C.DATA.train.point_scale   = 1.0         # 不知道干啥用的
 
 # For data augmentation
-_C.DATA.train.disable       = False       # Disable this dataset or not
+_C.DATA.train.disable       = False       # Disable this dataset or not 
 _C.DATA.train.distort       = False       # Whether to apply data augmentation
 _C.DATA.train.scale         = 0.0         # Scale the points
 _C.DATA.train.uniform       = False       # Generate uniform scales
 _C.DATA.train.jitter        = 0.0         # Jitter the points
-_C.DATA.train.interval      = (1, 1, 1)   # Use interval&angle to generate random angle
-_C.DATA.train.angle         = (180, 180, 180)
+_C.DATA.train.interval      = (1, 1, 1)   # Use interval to generate random angle
+_C.DATA.train.angle         = (180, 180, 180) # Use angle to generate random angle
 
 # For data loading
-_C.DATA.train.location      = ''          # The data location
-_C.DATA.train.filelist      = ''          # The data filelist
-_C.DATA.train.batch_size    = 32          # Training data batch size
+_C.DATA.train.location      = ''          # The data location | 数据集的位置
+_C.DATA.train.filelist      = ''          # The data filelist | 数据集中样本的索引
+_C.DATA.train.batch_size    = 32          # Training data batch size 
 _C.DATA.train.num_workers   = 8           # Number of workers to load the data
-_C.DATA.train.shuffle       = False       # Shuffle the input data
-_C.DATA.train.in_memory     = False       # Load the training data into memory
+_C.DATA.train.shuffle       = False       # Shuffle the input data | 是否打乱输入数据
+_C.DATA.train.in_memory     = False       # Load the training data into memory | 是否将数据加载到内存中
 
 
-_C.DATA.test = _C.DATA.train.clone()
+_C.DATA.test = _C.DATA.train.clone() # 我们先把 train 的参数拷贝给 test 一份
 
 
-# MODEL related parameters
-_C.MODEL = CN()
+# MODEL related parameters | 模型相关的参数
+_C.MODEL = CN() # 嵌套使用
 _C.MODEL.name               = ''          # The name of the model
-_C.MODEL.depth              = 5           # The input octree depth
+_C.MODEL.depth              = 5           # The input octree depth 
 _C.MODEL.full_depth         = 2           # The input octree full depth layer
 _C.MODEL.depth_out          = 5           # The output feature depth
 _C.MODEL.channel            = 3           # The input feature channel
-_C.MODEL.factor             = 1           # The factor used to widen the network
-_C.MODEL.nout               = 40          # The output feature channel
-_C.MODEL.resblock_num       = 3           # The resblock number
+_C.MODEL.factor             = 1           # The factor used to widen the network | 用于拓宽网络的因子
+_C.MODEL.nout               = 40          # The output feature channel | 输出特征通道
+_C.MODEL.resblock_num       = 3           # The resblock number | resblock 的个数
 _C.MODEL.resblock_type      = 'bottleneck'# Choose from 'bottleneck' and 'basic
 _C.MODEL.bottleneck         = 4           # The bottleneck factor of one resblock
 _C.MODEL.dropout            = (0.0,)      # The dropout ratio
 
-_C.MODEL.upsample           = 'nearest'   # The method used for upsampling
-_C.MODEL.interp             = 'linear'    # The interplation method: linear or nearest
-_C.MODEL.nempty             = False       # Perform Octree Conv on non-empty octree nodes
+_C.MODEL.upsample           = 'nearest'   # The method used for upsampling | 上采样方法
+_C.MODEL.interp             = 'linear'    # The interplation method: linear or nearest | 插补方法：线性 or 最近邻？
+_C.MODEL.nempty             = False       # Perform Octree Conv on non-empty octree nodes | 在非空的八叉树节点上执行八叉树Conv
 _C.MODEL.sync_bn            = False       # Use sync_bn when training the network
-_C.MODEL.use_checkpoint     = False       # Use checkpoint to save memory
-_C.MODEL.find_unused_parameters = False   # Used in DistributedDataParallel
+_C.MODEL.use_checkpoint     = False       # Use checkpoint to save memory | 是否使用 checkpoint 来节省内存
+_C.MODEL.find_unused_parameters = False   # Used in DistributedDataParallel 
 
 # loss related parameters
-_C.LOSS = CN()
+_C.LOSS = CN() # 嵌套使用
 _C.LOSS.name                = ''          # The name of the loss
-_C.LOSS.num_class           = 40          # The class number for the cross-entropy loss
+_C.LOSS.num_class           = 40          # The class number for the cross-entropy loss 
 _C.LOSS.weights             = (1.0, 1.0)  # The weight factors for different losses
 _C.LOSS.label_smoothing     = 0.0         # The factor of label smoothing
 
+# MXQ 2022.07.31
+_C.LOSS.loss_type           = 'possion_grad_loss'
 
 # backup the commands
 _C.SYS = CN()
